a5d55d84aa122fdf07c3887320ccde78567e9176
SaiyemBejoy <jsbejoy96@gmail.com>
2020-07-09 23:59:14 +0600

    Encapsulation implementation added

---
+++ Encapsulation/Encapsulation/Printer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulation
+{
+    class Printer
+    {
+        private int _tonerLevel;
+        private int _pagesPrinted;
+        private bool _duplex;
+
+        public Printer(int tonerLevel, bool duplex)
+        {
+            if(_tonerLevel > -1 && _tonerLevel <= 100)
+            {
+                _tonerLevel = tonerLevel;
+            }
+            else
+            {
+                _tonerLevel = -1;
+            }
+            _duplex = duplex;
+            _pagesPrinted = 0;
+        }
+
+
+        public int addToner(int tonerAmount)
+        {
+            if(tonerAmount > 0 && tonerAmount <= 100)
+            {
+                if(_tonerLevel + tonerAmount > 100)
+                {
+                    return -1;
+                }
+                _tonerLevel += tonerAmount;
+                return _tonerLevel;
+            }
+             return -1;
+        }
+
+        public int printPages(int pages)
+        {
+            int pagesToPring = pages;
+            if (_duplex)
+            {
+                pagesToPring = (pages / 2) + (pages % 2);
+                Console.WriteLine("Printing in duplex mode");
+            }
+            _pagesPrinted += pagesToPring;
+            return pagesToPring;
+        }
+        public int PagesPrinted { get => _pagesPrinted; }
+    }
+}
---
+++ Encapsulation/Encapsulation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var printer = new Printer(50, true);
+
+            Console.WriteLine("Initial page count = " + printer.PagesPrinted);
+
+            int pagesPrinted = printer.printPages(4);
+            Console.WriteLine("pages printed was " + pagesPrinted + " new total print count for printer = " + printer.PagesPrinted);
+            pagesPrinted = printer.printPages(2);
+            Console.WriteLine("pages printed was " + pagesPrinted + " new total print count for printer = " + printer.PagesPrinted);
+        }
+    }
+}
